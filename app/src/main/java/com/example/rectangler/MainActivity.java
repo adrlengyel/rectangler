ea83c554f78c93d0139589f9b5a474bea6fdb874
Adrián Lengyel <82450357+adrlengyel@users.noreply.github.com>
2022-02-02 18:08:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/rectangler/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.rectangler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnPlay, btnOptions, btnQuit;
+    private TextView tvTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        inicialize();
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnOptions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnQuit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAndRemoveTask();
+            }
+        });
+
+    }
+
+    private void inicialize(){
+
+        btnPlay = (Button) findViewById(R.id.btnPlay);
+        btnOptions = (Button) findViewById(R.id.btnOptions);
+        btnQuit = (Button) findViewById(R.id.btnQuit);
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+
+    }
+}
